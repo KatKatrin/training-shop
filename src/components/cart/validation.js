1e8e8244f738c77983022fecddda28ece3d159ba
ваше имя <ekat@gmail.com>
2022-04-19 15:05:44 +0300

    _7

--- src/components/cart/validation.js
+++ src/components/cart/validation.js
@@ -10,34 +10,34 @@ export const validationDelivery = () => {
                .matches(/^\+375\([25,29,33,44]/, 'Неверный код')
                .max(15, 'Максимум 9 символов для заполнения')
                .min(15, 'Минимум 9 символов для заполнения')
-               .required('Обязательное поле!'),
+               .required('Поле должно быть заполнено'),
       email: Yup.string()
               .email('Неправильный email адрес')
               .matches(/\.[a-z, A-Z]{2,63}$/, 'Неверный домен')
-              .required('Обязательное поле!'),
+              .required('Поле должно быть заполнено'),
       country: Yup.string()
               .matches(/\D/gm, "Неверные данные")
-              .required('Обязательное поле!'),
+              .required('Поле должно быть заполнено'),
       city: Yup.string()
               .when('deliveryMethod', {
                 is: (deliveryMethod) => (deliveryMethod === 'pickup from post offices' || deliveryMethod === 'express delivery'),
-                then: Yup.string().matches(/\D/gm, "Неверные данные").required('Обязательное поле!') }),
+                then: Yup.string().matches(/\D/gm, "Неверные данные").required('Поле должно быть заполнено') }),
       street: Yup.string()
               .when('deliveryMethod', {
                 is: (deliveryMethod) => (deliveryMethod === 'pickup from post offices' || deliveryMethod === 'express delivery'),
-                then: Yup.string().matches(/\D/gm, "Неверные данные").required('Обязательное поле!') }),
+                then: Yup.string().matches(/\D/gm, "Неверные данные").required('Поле должно быть заполнено') }),
       house: Yup.string()
             .when('deliveryMethod', {
               is: (deliveryMethod) => (deliveryMethod === 'pickup from post offices' || deliveryMethod === 'express delivery'),
-              then: Yup.string().required('Обязательное поле!') }),
+              then: Yup.string().required('Поле должно быть заполнено') }),
       postcode: Yup.string()
                   .when('deliveryMethod', {
                   is: 'pickup from post offices',
-                  then: Yup.string().matches(/[0-1]/gm, "Неверные данные").required('Обязательное поле!') }),
+                  then: Yup.string().matches(/[0-1]/gm, "Неверные данные").required('Поле должно быть заполнено') }),
       storeAddress: Yup.string()
                     .when('deliveryMethod', {
                       is: 'store pickup',
-                      then: Yup.string().required('Поле должно быть заполнено!')
+                      then: Yup.string().required('Поле должно быть заполнено')
                                         .max(24, 'Магазин не найден') }),
       terms: Yup.boolean()
               .required('Необходимо согласие')
@@ -55,9 +55,9 @@ export const validationPayment = () => {
         .when('paymentMethod', {
           is: paymentMethod => (paymentMethod === 'visa' || paymentMethod === 'master'),
           then: Yup.string()
-                .matches(/\d/gm, "Введите цифры")
-                .length(16, 'Введите 16 символов')
-                .required('Обязательное поле!'),
+                .matches(/\d/gm, "Проверьте правильность введенных данных")
+                .length(16, 'Проверьте правильность введенных данных')
+                .required('Поле должно быть заполнено'),
           }),   
 
       cardDate:Yup.string()
@@ -65,7 +65,7 @@ export const validationPayment = () => {
             is: paymentMethod => (paymentMethod === 'visa' || paymentMethod === 'master'),
             then: Yup.string()
               .matches(/(([0]{1}[1-9]{1}|[1]{1}[1-2]{1}).[2-4]{1}[3-9]{1})|(([0]{1}[4-9]{1}|[1]{1}[1-2]{1}).[2]{1}[2]{1})/, "Неверная дата")
-              .required('Обязательное поле!'),
+              .required('Поле должно быть заполнено'),
           }),
 
       cardCVV:Yup.string()
@@ -73,7 +73,7 @@ export const validationPayment = () => {
             is: paymentMethod => (paymentMethod === 'visa' || paymentMethod === 'master'),
             then: Yup.string()
                   .matches(/^[0-9,*]+$/, 'Введите цифры')
-                  .required('Обязательное поле!'),
+                  .required('Поле должно быть заполнено'),
           }),   
               
       cashEmail:Yup.string()
@@ -82,7 +82,7 @@ export const validationPayment = () => {
             then: Yup.string()
                       .email('Неправильный email адрес')
                       .matches(/\.[a-z, A-Z]{2,63}$/, 'Неверный домен')
-                      .required('Обязательное поле!'),
+                      .required('Поле должно быть заполнено'),
           }),
     }))
 }
